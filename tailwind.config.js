46a0deff6d5d55d4b54f20389cadb270d1074025
codyscott1 <codywaukee@gmail.com>
2021-12-08 14:41:48 -0600

    add typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-const green = {
-  primary: "#95BF47",
-  secondary: "#B9DF74",
-};
-const blue = {
-  focus: "#458FFF",
-  primary: "#1134B1",
-  secondary: "#17349C",
-  sky: "#79DFFF",
-  navy: "#183397",
-};
-const white = {
-  DEFAULT: "#FFFFFF",
-};
-const ink = "#000000";
-const gray = {
-  DEFAULT: "#D2D5D9",
-  1: "#3D3F40",
-  2: "#8D9091",
-  3: "#CCCCCC",
-  4: "#212326",
-  5: "#F4F4F4",
-};
-const red = {
-  1: "#E96A4E",
-  2: "#E73E4D",
-};
-const shopPayPurple = "#5A31F4";
-
 module.exports = {
   purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   mode: "jit",
@@ -82,53 +53,101 @@ module.exports = {
       gridTemplateColumns: {
         checkout: " 100px auto;",
       },
-      colors: {
-        primary: green.primary,
-        white,
-        green,
-        ink,
-        gray,
-        red,
-        blue,
-        "shop-pay-purple": shopPayPurple,
-      },
-      fontFamily: {
-        title: ["Apfel Grotezk Bold"],
-        primary: ["Apfel Grotezk"],
-        paragraph: ["Apfel Grotezk"],
-        shopfiysans: ["Shopify Sans"],
-      },
+      colors: {},
+
       fontSize: {
-        tn: ["9px", "12px"],
-        xsm: ["12px", "16.8px"],
-        sm: ["14px", "16.8px"],
-        base: ["16px", "22.4px"],
-        lg: ["20px", "23px"],
-        xl: ["28px", "33.6px"],
-        "2xl": ["42px", "42px"],
-        "3xl": ["58px", "52.2px"],
-        "4xl": ["80px", "75.2px"],
-        "4_5xl": ["80px", "72px"],
-        "5xl": ["120px", "108px"],
-        mbase: ["16px", "20.8px"],
-        mlg: ["18px", "26px"],
-        mxl: ["22px", "26.4px"],
-        m2xl: ["24px", "24px"],
-        m3xl: ["28px", "33.6px"],
-        m4xl: ["34px", "34px"],
-        m5xl: ["42px", "37.8px"],
-        m6xl: ["48px", "52px"],
-        about: ["24px", "33.6px"],
+        h1: [
+          "64px",
+          {
+            letterSpacing: "-2px",
+            lineHeight: "66px",
+          },
+        ],
+        h2: [
+          "48px",
+          {
+            letterSpacing: "-1px",
+            lineHeight: "52px",
+          },
+        ],
+        h3: [
+          "40px",
+          {
+            letterSpacing: "-1px",
+            lineHeight: "48px",
+          },
+        ],
+        h4: ["32px", "48px"],
+        h5: ["24px", "32px"],
+        h6: ["16px", "20.8px"],
+        "body-large": ["16px", "22px"],
+        body: ["14px", "20px"],
+        caption: ["12px", "18px"],
+        "util-l": [
+          "14px",
+          {
+            letterSpacing: "1px",
+            lineHeight: "14px",
+          },
+        ],
+        util: [
+          "12px",
+          {
+            letterSpacing: "1px",
+            lineHeight: "12px",
+          },
+        ],
+        "m-h1": [
+          "48px",
+          {
+            letterSpacing: "-1px",
+            lineHeight: "50px",
+          },
+        ],
+        "m-h2": [
+          "38px",
+          {
+            letterSpacing: "-1px",
+            lineHeight: "42px",
+          },
+        ],
+        "m-h3": [
+          "32px",
+          {
+            letterSpacing: "-1px",
+            lineHeight: "36px",
+          },
+        ],
+        "m-h4": ["24px", "32px"],
+        "m-h5": ["18px", "24px"],
+        "m-h6": ["14px", "19.6px"],
+        "m-body-large": ["14px", "20px"],
+        "m-body": ["12px", "20px"],
+        "m-caption": ["12px", "18px"],
+        "m-util-l": [
+          "14px",
+          {
+            letterSpacing: "2px",
+            lineHeight: "14px",
+          },
+        ],
+        "m-util": [
+          "12px",
+          {
+            letterSpacing: "1px",
+            lineHeight: "12px",
+          },
+        ],
       },
-      gradientColorStops: (theme) => ({
-        ...theme("colors"),
-        "primary-start": blue.sky,
-        "primary-end": blue.primary,
-      }),
-      backgroundColor: (theme) => ({
-        ...theme("colors"),
-        primary: green.primary,
-      }),
+      //   gradientColorStops: (theme) => ({
+      //     ...theme("colors"),
+      //     "primary-start": blue.sky,
+      //     "primary-end": blue.primary,
+      //   }),
+      //   backgroundColor: (theme) => ({
+      //     ...theme("colors"),
+      //     primary: green.primary,
+      //   }),
       backgroundImage: {
         "blob-image": "url('/src/assets/images/blob.png')",
         "blob-corner": "url('/src/assets/images/blobcorner2.png')",
